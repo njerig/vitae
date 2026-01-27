bbaf4a50fe729cace04feb4515635478619f9d8b
bercev <ercevik.berat@gmail.com>
2026-01-26 21:25:14 -0800

    Linter

--- app/mockdata/page.tsx
+++ app/mockdata/page.tsx
@@ -1,10 +1,10 @@
-import { SignedIn, SignedOut } from "@clerk/nextjs";
-import { auth, currentUser } from "@clerk/nextjs/server";
-import Image from "next/image";
+import { SignedIn, SignedOut } from "@clerk/nextjs"
+import { auth, currentUser } from "@clerk/nextjs/server"
+import Image from "next/image"
 
 export default async function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
       <SignedIn>
         <p>Hello, you are signed in</p>
       </SignedIn>
@@ -12,5 +12,5 @@ export default async function Home() {
         <p>SHOULD NOT be able to see this page. This text shouldnt even be able to appear</p>
       </SignedOut>
     </div>
-  );
+  )
 }
