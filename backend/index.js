4586f1188dc3b3cd5709fd5dcf58121559cee5e0
bercev <ercevik.berat@gmail.com>
2026-01-29 21:56:15 -0800

    Created GET and PUT routes

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,66 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+// set up postgres connectivity
+const { Pool } = require("pg");
+const POOL = new Pool({ connectionString: process.env.DATABASE_URL });
+
 app.use(cors());
 app.use(express.json());
 
-app.get("/health", (_req, res) => res.json({ ok: true }));
+
+
+
+// basic apis to play around with
+app.get("/api/health", (_req, res) => res.json({ ok: true }));
 
 app.get("/api/me", (_req, res) => {
   res.json({ id: "demo", name: "Demo User" });
 });
 
+// GET /canon/:userId
+app.get("/api/canon/:userId", async (req,res) => {
+  try {
+     const { userId } = req.params;
+
+     const { rows } = await POOL.query(
+       `
+        SELECT id, user_id, item_type, title, position, content, created_at, updated_at
+        FROM canon_items
+        WHERE user_id = $1
+        ORDER BY item_type, position, created_at;
+      `, [userId],);
+
+     res.json(rows);
+  } catch (error) {
+     console.error(`Server error: ${error}`);
+     res.status(500).json({ error: "Server error" });
+  }
+});
+
+// PUT /canon/:userId
+app.put("/api/canon/:userId", async (req,res) => {
+  try {
+     const { userId } = req.params;
+
+     const { rows } = await POOL.query(
+       `
+        SELECT id, user_id, item_type, title, position, content, created_at, updated_at
+        FROM canon_items
+        WHERE user_id = $1
+        ORDER BY item_type, position, created_at;
+      `, [userId],);
+
+     res.json(rows);
+  } catch (error) {
+     console.error(`Server error: ${error}`);
+     res.status(500).json({ error: "Server error" });
+  }
+});
+
+
 const port = Number(process.env.PORT ?? 8000);
 app.listen(port, () => {
   console.log(`Express API listening on http://localhost:${port}`);
 });
+
