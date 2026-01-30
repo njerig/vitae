04823878f2a8d61c494f410b9c98e722dcee9efb
bercev <ercevik.berat@gmail.com>
2026-01-29 19:52:46 -0800

    Added .css module to fix globals.css warning issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import { type Metadata } from "next"
 import { ClerkProvider, SignInButton, SignUpButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import { Geist, Geist_Mono } from "next/font/google"
-// @ts-ignore
 import Link from "next/link"
 import "./globals.css"
 
