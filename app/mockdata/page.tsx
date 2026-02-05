ad5d21df54c889a13e0c5540416ea5d510a24328
bercev <ercevik.berat@gmail.com>
2026-02-04 16:53:21 -0800

    Fixed build issues

--- app/mockdata/page.tsx
+++ app/mockdata/page.tsx
@@ -1,6 +1,5 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs"
 import { auth, currentUser } from "@clerk/nextjs/server"
-import Image from "next/image"
 
 export default async function Home() {
 
@@ -11,7 +10,7 @@ export default async function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <SignedIn>
-        <p>Hello, you are signed in, you are in MOCKDATA ]]]</p>
+        <p>Hello, you are signed in, you are in MOCKDATA</p>
         <p>{userName}</p>
       </SignedIn>
       <SignedOut>
